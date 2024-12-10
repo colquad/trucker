9546ff464a406acc0a4050389b11e597d0a005b1
Colin Quade <cjjq1001@gmail.com>
2024-12-09 23:36:36 -0500

    Removed info

--- web-app/src/lib/db/mysql.ts
+++ web-app/src/lib/db/mysql.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
     
     export const mysqlconn = await mysql.createConnection({ 
-        host: "team21-rds.cobd8enwsupz.us-east-1.rds.amazonaws.com",
-        user: "admin",
-        password: "Monkey3750*",
-        database: "team21"
+        host: "",
+        user: "",
+        password: "",
+        database: ""
     });
